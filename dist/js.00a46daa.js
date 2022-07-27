8db463fd035744c4f74221e2cf9177f0a860d4fc
yadroudjlinda <lindayadroudj@imadrassa.com>
2022-07-27 12:34:33 +0100

    add changes

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -17308,7 +17308,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51033" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51206" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
